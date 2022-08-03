6142a0828cae4266b21e7218ddc579d3eb49ecb8
Juan Jose <juan.sepulveda@edu.uag.mx>
2022-08-03 13:38:24 -0600

    admin

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -20,6 +20,9 @@ class QuestionAdmin(admin.ModelAdmin):
 	# Cambiamos el orden de los campos
 	fields = ["pub_date", "question_text"]
 	inlines = [ChoiceInline]
+	list_display = ("question_text", "pub_date", "was_published_recently")
+	list_filter = ["pub_date"]
+	search_fields = ["question_text"]
 
 
 # Register your models here.
