151ae032428e2cd94c3c5b3536a775d97463e5ec
Juan Jose <juan.sepulveda@edu.uag.mx>
2022-08-03 13:29:45 -0600

    clase11

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -3,7 +3,24 @@
 from django.contrib import admin
 
 # Models
-from .models import Question
+from .models import Question, Choice
+
+class ChoiceInline(admin.StackedInline):
+	"""
+	Clase para conbinar la creación de un modelo con otro
+	"""
+	model = Choice
+	extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+	""" 
+	clase que nos sirve para personalizar como se ve el modelo en la pagina de admin
+	"""
+	# Cambiamos el orden de los campos
+	fields = ["pub_date", "question_text"]
+	inlines = [ChoiceInline]
+
 
 # Register your models here.
-admin.site.register(Question)
+admin.site.register(Question, QuestionAdmin)
